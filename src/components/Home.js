494cc56d59aaf0dcc6d63cc56dff9f0bef38ff21
Sugut <sugutvictorine@gmail.com>
2022-10-10 13:31:00 +0300

    done

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+function Home() {
+
+  return (
+     <div className='home'>
+           {/* <video src="/NetflixIntro.mp4" width="320" height="240" autoPlay="true"/> 
+           <iframe width="100%" height="100%" src="/NetflixIntro.mp4"></iframe>  */}
+      </div>
+        
+  )
+}
+
+export default Home
