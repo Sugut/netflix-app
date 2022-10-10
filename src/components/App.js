0d2018c4107ff433e438206a03facedb4767fea3
Sugut <sugutvictorine@gmail.com>
2022-10-10 13:53:29 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import '../App.css';
 import React,{useEffect, useState} from "react";
 import { Route, Routes} from "react-router-dom";
 import { Navbar } from "./Navbar";
@@ -10,48 +10,53 @@ import Cast from "./Cast"
 import Home from "./Home"
 import Trending from './Trending';
 
+
 function App() {
   const[movies, setMovies]=useState([])
   const [search, setSearch]= useState("")
+  const [shows, setShows]= useState([])
+  const [casts, setCasts]= useState([])
+  const [trends, setTrends]=useState([])
+ 
 
   const searchApi="https://api.themoviedb.org/3/search/movie?api_key=1bb8166c24cb38120e165753855c0acc&query="
+   useEffect(()=>{
+      fetch("https://api.themoviedb.org/3/movie/popular?api_key=1bb8166c24cb38120e165753855c0acc")
+      .then(r=>  r.json())
+      .then(data=> setMovies(data.results))
+   },[])
+   function handleChange(e){
+        setSearch(e.target.value)
+   }
+   function handleSubmit(e){
+        e.preventDefault()
 
-  useEffect(()=>{
-    fetch("https://api.themoviedb.org/3/movie/popular?api_key=1bb8166c24cb38120e165753855c0acc")
-    .then(r=>  r.json())
-    .then(data=> setMovies(data.results))
- },[])
- function handleChange(e){
-  setSearch(e.target.value)
-}
-function handleSubmit(e){
-  e.preventDefault()
-
-  if(search){
-  fetch(searchApi + search)
-  .then(r=>  r.json())
-  .then(data=> setMovies(data.results))
-  }
-  setSearch("")
-}
-useEffect(()=>{
-  fetch("https://api.themoviedb.org/3/tv/popular?api_key=1bb8166c24cb38120e165753855c0acc")
-  .then(r=>r.json())
-  .then(data=> setShows(data.results))
-      console.log(data)
- },[])
- useEffect(()=>{
-  fetch("https://api.themoviedb.org/3/person/popular?api_key=1bb8166c24cb38120e165753855c0acc")
-  .then(r=>r.json())
-  .then(data=> setCasts(data.results))
-},[])
-useEffect(()=>{
-  fetch("https://api.themoviedb.org/3/trending/all/day?api_key=1bb8166c24cb38120e165753855c0acc")
-  .then(r=>r.json())
-  .then(data=> setTrends(data.results))
-},[])
+        if(search){
+        fetch(searchApi + search)
+        .then(r=>  r.json())
+        .then(data=> setMovies(data.results))
+        }
+        setSearch("")
+   }
+   useEffect(()=>{
+    fetch("https://api.themoviedb.org/3/tv/popular?api_key=1bb8166c24cb38120e165753855c0acc")
+    .then(r=>r.json())
+    .then(data=> setShows(data.results))
+        console.log(data)
+   },[])
+   useEffect(()=>{
+     fetch("https://api.themoviedb.org/3/person/popular?api_key=1bb8166c24cb38120e165753855c0acc")
+     .then(r=>r.json())
+     .then(data=> setCasts(data.results))
+   },[])
+   useEffect(()=>{
+    fetch("https://api.themoviedb.org/3/trending/all/day?api_key=1bb8166c24cb38120e165753855c0acc")
+    .then(r=>r.json())
+    .then(data=> setTrends(data.results))
+  },[])
+   
   return (
-    <div className="App">
+    <div className="app">    
       <Navbar />
       <Search search={search} handleSubmit={handleSubmit} handleChange={handleChange} setSearch={setSearch}/>
       <Routes>
@@ -61,7 +66,7 @@ useEffect(()=>{
         <Route path="/casts" element={<Cast casts={casts}/>} /> 
         <Route path="/trending" element={<Trending trends={trends}/>}/>
       </Routes>
-    </div>
+    </div>  
   );
 }
 
